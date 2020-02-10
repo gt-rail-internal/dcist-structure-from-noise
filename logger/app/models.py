694ae70a44596f2035a402dd16dfb1e8e02ab79f
akshay-krishnan <akshay.krishnan.30@gmail.com>
2020-02-10 14:48:58 -0500

    logger using flask started

---
+++ logger/app/models.py
@@ -0,0 +1,11 @@
+from app import db
+
+class Response(db.Model):
+	user_id = db.Column(db.String(20), primary_key=True)
+	pretest_graph_type = db.Column(db.String(10), index=True)
+	pretest_graph = db.Column(db.String(300))
+	pretest_keys = db.Column(db.String(5000))
+	pretest_reactions = db.Column(db.String(45000))
+
+	def __repr__(self):
+		return '<Response from {} {}>'.format(self.id, self.reactions)
