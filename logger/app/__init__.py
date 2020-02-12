8d3d3fa487736ed3bb08915123777f7fd29f52ed
Ubuntu <ubuntu@ip-172-31-86-212.ec2.internal>
2020-02-12 14:20:22 +0000

    logging remotely

--- logger/app/__init__.py
+++ logger/app/__init__.py
@@ -3,11 +3,18 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+import logging
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+    app.logger.debug("logger initialized")
 
 from app import routes, models
--- logger/app/routes.py
+++ logger/app/routes.py
@@ -6,13 +6,17 @@ from app import db
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, world!"
+    app.logger.debug("Index function accessed from app")    
+    return "Hello, world!"
 
 @app.route('/pretest-data', methods=['POST'])
 def logData():
-	reponseDict = processing.responseDictFromString(request.data.decode('ASCII'))
-	print(reponseDict)
-	response = processing.responseFromDict(reponseDict)
-	db.session.add(response)
-	db.session.commit()
-	return reponseDict
+    app.logger.debug("logging function accessed from app")
+    app.logger.debug(request.data.decode('ASCII'))
+    reponseDict = processing.responseDictFromString(request.data.decode('ASCII'))
+    print(reponseDict)
+    response = processing.responseFromDict(reponseDict)
+    db.session.add(response)
+    db.session.commit()
+    app.logger.debug("exiting function")
+    return reponseDict
