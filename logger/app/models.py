55acac168cb78f14ab86a73f5853d7c0787b0fc5
Ubuntu <ubuntu@ip-172-31-86-212.ec2.internal>
2020-02-14 17:16:56 +0000

    logging to db fully working

--- logger/app/models.py
+++ logger/app/models.py
@@ -4,8 +4,8 @@ class Response(db.Model):
 	user_id = db.Column(db.String(20), primary_key=True)
 	pretest_graph_type = db.Column(db.String(10), index=True)
 	pretest_graph = db.Column(db.String(300))
-	pretest_keys = db.Column(db.String(5000))
-	pretest_reactions = db.Column(db.String(45000))
+	pretest_keys = db.Column(db.Text)
+	pretest_reactions = db.Column(db.Text)
 
 	def __repr__(self):
 		return '<Response from {} {}>'.format(self.user_id, self.pretest_reactions)
