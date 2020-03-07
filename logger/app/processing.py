a89e000c75756b4b06e976c0b22fceee064dc788
akshay-krishnan <akshay.krishnan.30@gmail.com>
2020-03-07 11:04:53 -0500

    logging change

--- logger/app/processing.py
+++ logger/app/processing.py
@@ -1,8 +1,8 @@
 from app.models import Response
+from app import app
 
 def responseDictFromString(response):
 	dataDict = {}
-	print("$", "a", "$", "b", "$")
 	for entry in response.split('::'):
 		if len(entry) > 1:
 			values = entry.split(', ')
@@ -23,13 +23,13 @@ def responseDictFromString(response):
 	return dataDict
 
 def responseFromDict(data_dict):
-	r = Response(user_id=data_dict['user_id'], pretest_graph_type=data_dict['pretest_graph_type'], pretest_graph=data_dict['pretest_graph'], pretest_keys=data_dict['pretest_keys'], pretest_reactions=data_dict['pretest_reactions'], pretest_key_status=data_dict['pretest_key_status'], pretest_token=data_dict['pretest_token'], pretest_status = 2)
-	print(r)
-	print("response is above")
+	r = Response(user_id=data_dict['user_id'], pretest_graph_type=data_dict['pretest_graph_type'], pretest_graph=data_dict['pretest_graph'], pretest_keys=data_dict['pretest_keys'], pretest_reactions=data_dict['pretest_reactions'], pretest_status = 2, pretest_key_status=data_dict['pretest_key_status'], pretest_token=data_dict['pretest_token'])
+	app.logger.debug(r)
+	app.logger.debug("response is above")
 	return r
 
 def newResponse(new_user_id):
 	r = Response(user_id = new_user_id)
-	print(r)
-	print("new entry in table above")
+	app.logger.debug(r)
+	app.logger.debug("new entry in table above")
 	return r
