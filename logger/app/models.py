51bdcb094fb73799c100495f7694027644571827
Ubuntu <ubuntu@ip-172-31-86-212.ec2.internal>
2020-03-07 03:15:07 +0000

    changes to model before launch

--- logger/app/models.py
+++ logger/app/models.py
@@ -1,11 +1,15 @@
 from app import db
 
 class Response(db.Model):
-	user_id = db.Column(db.String(20), primary_key=True)
-	pretest_graph_type = db.Column(db.String(10), index=True)
-	pretest_graph = db.Column(db.String(300))
-	pretest_keys = db.Column(db.Text)
-	pretest_reactions = db.Column(db.Text)
+    user_id = db.Column(db.String(20), primary_key=True)
+    pretest_graph_type = db.Column(db.String(10), index=True, default = "")
+    pretest_graph = db.Column(db.String(300), default = "")
+    pretest_keys = db.Column(db.Text, default = "")
+    pretest_reactions = db.Column(db.Text, default = "")
+    pretest_status = db.Column(db.Integer, default = 1)
+    pretest_key_status = db.Column(db.Text, default="")
+    pretest_token = db.Column(db.String(12), default= "")
 
-	def __repr__(self):
-		return '<Response from {} {}>'.format(self.user_id, self.pretest_reactions)
+    def __repr__(self):
+        return '<Response from {} {}>'.format(self.user_id, self.pretest_reactions)
+
